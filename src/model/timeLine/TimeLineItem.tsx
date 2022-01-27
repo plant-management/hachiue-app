f1a09b55222d590c07388c19a0cfb98b8352e673
yochimonji <yochimonji@gmail.com>
2022-01-27 10:39:52 +0000

    add タイムラインのCSSを調整

--- src/model/timeLine/TimeLineItem.tsx
+++ src/model/timeLine/TimeLineItem.tsx
@@ -12,17 +12,23 @@ import tailwind from "tailwind-rn";
 
 const TimeLineItem = (props: ListRenderItemInfo<TimeLineItemType>) => {
   return (
-    <View>
+    <View style={tailwind("mb-4")}>
       <View style={tailwind("flex-row justify-between")}>
-        <Text>{props.item.plantName}</Text>
-        <Text>{props.item.day}</Text>
+        <Text style={tailwind("text-base")}>{props.item.plantName}</Text>
+        <Text style={tailwind("text-base text-gray-600")}>
+          {props.item.day}
+        </Text>
       </View>
-      <TouchableOpacity style={tailwind("flex-row")}>
+      <TouchableOpacity
+        style={tailwind(
+          "flex-row items-center bg-white border border-gray-300 rounded-lg p-2"
+        )}
+      >
         <Image
           source={{ uri: props.item.characterImageUrl }}
           style={tailwind("w-16 h-16")}
         />
-        <Text>{props.item.comment}</Text>
+        <Text style={tailwind("text-base")}>{props.item.comment}</Text>
       </TouchableOpacity>
     </View>
   );
